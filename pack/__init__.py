5f499961ab99f3f7ddd30c9dc53571194db621af
dilfhuntress <68343776+dilfhuntress@users.noreply.github.com>
2020-07-27 15:29:50 +0300

    Update __init__.py

--- pack/__init__.py
+++ pack/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = 'secret stuff'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/web'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://user:123@localhost/web'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 manager = LoginManager(app)
